454668f6226b0763f9257f9fe80ad2589b1ce6da
lukas <akashare-creationgraphics@outlook.com>
2023-03-23 11:31:49 -0300

    atualizacao

---
+++ frontend/pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/globals.css'
+import dyanmic from 'next/dynamic'
+import '@solana/wallet-adapter-react-ui/styles.css'
+
+function MyApp({ Component, pageProps }) {
+  const WalletConnectionProvider = dyanmic(
+    () => import ('../context/WalletConnectionProvider'),
+    { ssr:false },
+  )
+  return (
+     <WalletConnectionProvider>
+       <Component {...pageProps} />
+     </WalletConnectionProvider>
+  )
+ 
+}
+
+export default MyApp
